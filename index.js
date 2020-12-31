c78432bed7af635ca836b77329753a4bd1ebb0d8
Josiah Ayres <jayres@nz1.ibm.com>
2020-12-31 14:52:52 +1300

    Startup command

---
+++ index.js
@@ -0,0 +1,15 @@
+const concurrently = require("concurrently");
+
+const success = () => {
+	console.log("Started successfully");
+};
+
+const failure = () => {
+	console.error("Something went wrong");
+};
+
+concurrently([{ command: "npm run start", name: "nextjs" }], {
+	prefix: "name",
+	killOthers: ["failure", "success"],
+	restartTries: 3,
+}).then(success, failure);
